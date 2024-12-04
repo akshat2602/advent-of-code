bee8c1e50ea7c8a7dbcea264f5c49882b888657a
Akshat Sharma <akshatsharma2602@gmail.com>
2024-12-04 16:39:25 -0500

    still working on day 3 part 2

--- 2024/day-3/src/main.rs
+++ 2024/day-3/src/main.rs
@@ -1,7 +1,6 @@
 use regex::Regex;
 
 fn get_input(input: &str) -> Vec<&str> {
-    println!("{:?}", input);
     // Go over the input and take out anything of the form mul(i32,i32)
     let re = Regex::new(r"mul\(\d+,\d+\)").unwrap();
     let mut valid_input = Vec::new();
@@ -12,11 +11,37 @@ fn get_input(input: &str) -> Vec<&str> {
     valid_input
 }
 
+fn get_valid_input(input: &str) -> Vec<&str> {
+    let mut count = 0;
+    let mut valid_input = Vec::new();
+    let mut curr_thing = 0; // 0 for do, 1 for don't
+    let re = Regex::new(r"do\(\)|don't\(\)").unwrap();
+    let mut prev_end: usize = 0;
+    for matc in re.find_iter(input) {
+        count += 1;
+        let curr_start = matc.start();
+        let curr_end = matc.end();
+        if curr_thing == 0 {
+            valid_input.append(&mut get_input(&input[prev_end + 1..curr_start]));
+            curr_thing = 1;
+            prev_end = curr_end;
+        } else {
+            prev_end = curr_end;
+            curr_thing = 0;
+        }
+    }
+    if curr_thing == 0 {
+        valid_input.append(&mut get_input(&input[prev_end + 1..]));
+    }
+    println!("{:?}", count);
+    valid_input
+}
+
 fn main() {
     let path = std::path::Path::new("input.txt");
     let input = std::fs::read_to_string(path).unwrap();
 
-    let valid_input = get_input(&input);
+    let valid_input = get_valid_input(&input);
 
     let sum: i32 = valid_input.iter().map(|x| get_mul(*x)).sum();
 
